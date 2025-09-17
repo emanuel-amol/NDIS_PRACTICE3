7462bbf753c7ef077529db5ce3581dfe84a6e630
104515032 <104515032@student.swin.edu.au>
2025-09-17 20:34:30 +1000

    initial

---
+++ frontend/src/index.tsx
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+function App(){ return <div className='p-6 text-xl font-semibold'>CRA + Tailwind v3 is working</div>; }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
